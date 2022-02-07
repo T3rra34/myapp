ea40c68f20afff3315c7d00cf325519ee2492430
T3rra34 <T3rra34@github.com>
2022-02-07 10:38:15 +0200

    tehtud

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const fetch = require('node-fetch')
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(_dirname, 'views'))
+
+const key = '9e2145bbf1391d096fb2e91365d0313c';
+
+const getWeatherDataPromise = (url) => {
+	return new Promise((resolve, reject) =>{
+		fetch(url)
+		.then(responce => {
+			return responce.json()
+		})
+		.then(data => {
+			let description = data.weather[0].description
+			let city = data.name
+			let temp = Math.round(parseFloat(data.main.temp)-273.15)
+			let result = {
+				description: description,
+				city: city,
+				temp: temp,
+				error: null
+			}
+			resolve(result)
+		})
+		.catch(error => {
+			reject(error)
+		})
+	})
+}
+
+app.get('/', function(req, res) {
+	let city = 'Tartu'
+	let url =`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`
+	getWeatherDataPromise(url)
+	.then(data => {
+		res.render('index', data)
+	})
+})
+
+app.post('/', function(req, res){
+	let city = req.body.cityname
+	url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`
+	getWeatherDataPromise(url)
+	.then(data => {
+		res.render('index', data)
+	})
+})
+
+app.all('/', function(req, res) {
+	let city
+	if(req.method == 'GET'){
+		city = 'Tartu'
+	}
+	if(req.method == 'POST'){
+		city = req.body.cityname
+	}
+	let url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`
+	getWeatherDataPromise(url)
+	.then(data => {
+		res.render('index', data)
+	})
+	.catch(error => {
+		res.render('index', {error: 'Problem with getting data, try again'})
+	})
+})
+
+app.listen(3000)
